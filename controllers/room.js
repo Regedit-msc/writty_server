b7bd76a604011389aeec1eaced3f436db6c74d67
Regedit-msc <efusanyaae@gmail.com>
2021-07-24 13:43:39 +0100

    small change

--- controllers/room.js
+++ controllers/room.js
@@ -8,7 +8,7 @@ const getRooms = async (req, res, next) => {
     const newArr = [];
     for (let i = 0; i < rooms.length; i++) {
         const lastMessage = (await Message.find({ room: rooms[i].roomID }).populate("user").sort({ createdAt: -1 }).limit(1))[0]
-        const notYou = (rooms[i].participants.filter(u => u.user._id != username))[0];
+        const notYou = (rooms[i].participants.filter(u => u?.user?._id != username))[0];
         newArr.push({ lastMessage: _.pick(lastMessage, ["_id", "user.username", "body", "type", "format", "createdAt"]), userTOChat: _.pick(notYou, ["_id", "user.username", "user.profileImageUrl"]) });
     }
     const arrToSend = newArr.sort(function (a, b) {
@@ -18,6 +18,7 @@ const getRooms = async (req, res, next) => {
         if (keyA > keyB) return -1;
         return 0;
     });
+    console.log(arrToSend);
     res.status(200).json({ message: arrToSend, success: true });
 }
 
